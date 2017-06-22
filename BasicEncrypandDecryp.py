b15bca46e138a71f435bdadf1a3412eae4264feb
rafe1486 <songrit.k@mail.kmutt.ac.th>
2017-06-22 15:39:51 +0900

    Mearge with Notebook

---
+++ BasicEncrypandDecryp.py
@@ -0,0 +1,58 @@
+from Crypto.PublicKey import RSA
+import sys
+import main
+
+bits = 32
+message = '123456789'
+print("msg :",message)
+
+def checksize(keySize, messageSize):
+    messageSize *= 8
+    keySize += 1
+    if(keySize >= messageSize):
+        return True
+    if(keySize < messageSize):
+        return False
+
+#main.saveKey(main.createKeyPair(bits))
+with open('Publickey.pem', 'rb') as file:
+    keydata = file.read()
+    rsakey = RSA.importKey(keydata)
+    keySize = rsakey.size()
+    messageSize = len(message)
+    message = message.encode()
+
+    if (checksize(keySize, messageSize)):
+        ciphertext = rsakey.encrypt(message, 0)[-1]
+    else:
+        keySize +=1
+        keySize /=8
+        keySize =int(keySize)
+        ciphertext =b''
+        message = [message[i:i+keySize] for i in range(0, len(message), keySize)]
+        for msg in message :
+            ciphertext += rsakey.encrypt(msg,0)[-1]
+    with open('Ciper','wb') as text_file:
+       text_file.write(ciphertext)
+
+
+with open('PrivateKey.pem', 'rb') as file:
+    keydata = file.read()
+    rsakey = RSA.importKey(keydata)
+    keySize = rsakey.size()
+
+    with open('Ciper','rb') as text_file:
+       ciphertext = text_file.read()
+    cipherSize = len(ciphertext)
+    
+    if (checksize(keySize, cipherSize)):
+       text = rsakey.decrypt(ciphertext)
+    else:
+        keySize +=1
+        keySize /=8
+        keySize =int(keySize)
+        text =b''
+        ciphertext = [ciphertext[i:i+keySize] for i in range(0, len(ciphertext), keySize)]
+        for ctext in  ciphertext :
+            text += rsakey.decrypt(ctext)
+        print(text.decode('utf-8', 'ignore'))
